bded7b8fb3b44cbb3a6aeec7b3ad0b4a5a8eeb20
Oussama Hichou <usmhic@gmail.com>
2026-09-06 19:40:54 +0100

    Removed DB creator

--- api/src/main/java/com/freesolo/api/FreeSoloApiApplication.java
+++ api/src/main/java/com/freesolo/api/FreeSoloApiApplication.java
@@ -1,6 +1,5 @@
 package com.freesolo.api;
 
-import com.freesolo.api.config.DatabaseCreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,9 +7,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class FreeSoloApiApplication {
+
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(FreeSoloApiApplication.class);
-        app.addListeners(new DatabaseCreator());
-        app.run(args);
+        SpringApplication.run(FreeSoloApiApplication.class, args);
     }
 }
